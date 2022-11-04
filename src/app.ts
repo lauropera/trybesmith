433a5755056e39e0411bdebe201466b21e58ac2e
Lauro Pereira <lauropera01@gmail.com>
2022-11-04 15:52:33 -0300

    Estrutura inicial do projeto

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import httpErrorMiddleware from './middlewares/http.error.middleware';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(httpErrorMiddleware);
 
 export default app;
