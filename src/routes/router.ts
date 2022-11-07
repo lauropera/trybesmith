4dc651f74d8a5fd59c044db262853ad9bb602f81
Lauro Pereira <lauropera01@gmail.com>
2022-11-07 14:34:28 -0300

    Implementando swagger

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,14 +1,19 @@
-import { Router } from 'express';
+import express from 'express';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from '../swagger.json';
+
 import loginRouter from './login.routes';
 import userRouter from './user.routes';
 import orderRouter from './order.routes';
 import productRouter from './product.routes';
 
-const routers = Router();
+const routers = express.Router();
 
 routers.use('/login', loginRouter);
 routers.use('/users', userRouter);
 routers.use('/orders', orderRouter);
 routers.use('/products', productRouter);
+routers.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export default routers;
